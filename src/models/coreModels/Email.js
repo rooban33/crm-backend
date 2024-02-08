aa6a74d5437b670ce926c6c75ed9c20d73213c13
rooban33 <shajithgunasekaran@gmail.com>
2024-02-08 23:33:15 +0530

    first commit

---
+++ src/models/coreModels/Email.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Allow all origins
+app.use(cors());
+
+const emailSchema = new mongoose.Schema({
+  removed: {
+    type: Boolean,
+    default: false,
+  },
+  enabled: {
+    type: Boolean,
+    default: true,
+  },
+  emailKey: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  emailName: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  emailVariables: {
+    type: Array,
+  },
+  emailBody: {
+    type: String,
+    required: true,
+  },
+  emailSubject: {
+    type: String,
+    required: true,
+  },
+  language: {
+    type: String,
+    default: 'us_en',
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Email', emailSchema);
---
+++ src/models/coreModels/Setting.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Allow all origins
+app.use(cors());
+const settingSchema = new mongoose.Schema({
+  removed: {
+    type: Boolean,
+    default: false,
+  },
+  enabled: {
+    type: Boolean,
+    default: true,
+  },
+  settingCategory: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  settingKey: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  settingValue: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+  valueType: {
+    type: String,
+    default: 'String',
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false,
+  },
+  isCoreSetting: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('Setting', settingSchema);
